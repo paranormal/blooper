2d5a0d099e6467b05cce48cf9b6c0fa7aa46a083
paranormal <paranormal@isgroup.com.ua>
2013-02-28 02:22:18 +0200

    temporary rescue for bad encoding

--- lib/blooper/input.rb
+++ lib/blooper/input.rb
@@ -5,13 +5,12 @@ module Blooper
 
     def initialize(flow = STDIN)
       @flow = flow
-      @flow.set_encoding('UTF-8')
     end
 
     def self.each(*args)
       Input.new(*args).flow.each do |line|
         line = Line.new(line)
-        yield Tuple.new(line.clean) if line.valid?
+        yield Tuple.new(line.clean) if line.valid?  rescue ArgumentError
       end
     end
 
